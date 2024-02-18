37631684e14573891ffddfcf96ed5e2c574af923
DAShteinberh <104132291+DAShteinberh@users.noreply.github.com>
2024-02-19 01:00:37 +0200

    Update finePayments.js
    add clearing of fields
    combining checks of the fine number and amount

--- js/finePayments.js
+++ js/finePayments.js
@@ -13,55 +13,34 @@ let buttonSubmit = document.getElementById("payFine");
 
 //Ця зміна містить всі дані які в нас зберігаються у файлі data
 let DB = data.finesData;
-var searchFineNumber =  DB.find(item => item.номер == fineNumber.value);
-var searchAmount =  DB.find(item => item.сума == amount.value);
 
 buttonSubmit.addEventListener('click',payFine);
-
 function payFine() {
 
     var regPassport = /^[А-ЩЬЮЯҐЄІЇ]{2}\d{6}$|^\d{9}$/g;
     var regCard = /^[4-6]+\d{15}$/g;
     var regCVV = /^\d{3}$/g;
 
-
-    if (fineNumber.value !== searchFineNumber.номер) {
-         alert("Номер штрафу не співпадає");
-        // console.log(searchFineNumber)
+    if (DB.find((v)=>{return (v.номер === fineNumber.value) && (v.сума === +amount.value)}) === undefined) {
+         alert("Номер штрафу або Сума не співпадає");
     } else if (!regPassport.test(passport.value)) {
         alert ("Не вірні паспортні дані");
     } else if (!regCard.test(creditCardNumber.value)) {
          alert ("Не вірний номер кредитної картки");
     } else if (!regCVV.test(cvv.value)) {
          alert ("Не вірний cvv");
-    } else if (amount.value !== searchAmount.сума) {
-            alert("Сума штрафу не співпадає");
-                console.log(searchAmount)
     } else {
+        alert ("Штраф сплачено");
+        
         for (let i = DB.length; i--; ) {
-            if (DB[i].номер === fineNumber & DB[i].amount === amount) {
+            if (DB[i].номер === fineNumber.value && DB[i].сума === +amount.value) {
               DB.splice(i, 1);
         }
       } 
+        fineNumber.value = '';
+        passport.value = '';
+        creditCardNumber.value = '';
+        cvv.value = '';
+        amount.value = '';
     };
 };
-
-
-
-/**
-Вам необхідно реалізувати наступний функціонал.
-Зробити валідацію до всіх полів
-1. Номер та сума повинні бути однакові як в існуючого штрафу - якщо ні видавати
-alert "Номер не співпадає" або "Сума не співпадає"
-
-2. Паспортні дані у форматі - перші дві літери укр алфавіту, та 6 цифр.
-Якщо не співпадає то видавати alert "Не вірний паспортний номер"
-
-3. Номер кредитної карки 16 цифр -
-якщо не співпадає то видавати alert "Не вірна кредитна картка"
-
-4. cvv 3 цифри - якщо не співпадає то видавати alert "Не вірний cvv".
-
-Якщо валідація проходить успішно, то виконати оплату,
- тобто вам потрібно видалити обєкт з DB
- */
